575bdc55cb7db55d0d5d8957c2639b842af6e02b
priyasirohi09 <priyanshi.sirohi@scaler.com>
2025-09-30 15:54:16 +0530

    variables

---
+++ Class11 (Intro to JS)/script.js
@@ -0,0 +1,70 @@
+// console.log('Hello world')
+/*
+var age = 30;
+console.log(age) //30 
+console.log('age') //age 
+
+var message = 'Good Afternoon!!';
+console.log(message)
+
+var psp = 87.5 
+console.log(psp)
+
+var isPresent = false;
+console.log(isPresent)
+
+var isPresent = 600; //Redeclaration is allowed [Issue]
+console.log(isPresent)
+
+let userName = 'Priya'
+console.log(userName)
+
+// let userName = 'Lokesh' // Error :  'userName' has already been declared 
+
+
+const pi = 3.14
+console.log(pi)
+
+// const pi = 3.1415 // Error : 'pi' has already been declared 
+
+
+
+var num = 1000;
+num = 500;
+console.log(num)
+
+let num1 = 900
+num1  = 90 
+console.log(num1)
+
+const pi = 3.14
+pi = 3.1415  // Error: Assignment to constant variable.
+console.log(pi)
+
+*/
+
+let age 
+console.log(age)
+console.log(typeof age) // undefined
+
+
+
+age = 100;
+console.log(age) // 100
+console.log(typeof age) // number
+
+let userName = 'Lokesh'
+console.log(typeof userName) // string
+
+let isPresent = true 
+console.log(typeof isPresent) // boolean
+
+
+let empty = null 
+console.log(empty) // null
+console.log(typeof empty) // object
+
+
+// const pi  // Error : const must be initialised
+
+
