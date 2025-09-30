52b93c4513875dadd50b21be75e8a793161d7282
priyasirohi09 <priyanshi.sirohi@scaler.com>
2025-09-30 16:52:47 +0530

    js

---
+++ Class11 (Intro to JS)/concat.js
@@ -0,0 +1,8 @@
+console.log(5+3) // 8
+console.log('5' + 3) // 53 [Concatenation]
+console.log('5' + '3') // 53 [Concatenation]
+
+
+console.log('abc' - 2); //NaN [Not A Number]
+console.log('4' * '5'); // 20
+console.log('40' -  10 ); // 30
