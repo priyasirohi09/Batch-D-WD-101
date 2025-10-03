7b8f0432cbb3a64fc41cfd58a53bc206842fbc4f
priyasirohi09 <priyanshi.sirohi@scaler.com>
2025-10-03 15:10:32 +0530

    functions

---
+++ Class12 (Functions, loops)/script.js
@@ -0,0 +1,24 @@
+function greeting(username){
+  // console.log('Good Afternoon, ' + username)
+  console.log(`Good Afternoon, ${username}`)
+}
+
+let res = greeting('Priyanshi');
+console.log(res)
+
+// function that returns sum of two numbers
+function sumOfTwo(num1, num2){
+  // return (num1 + num2);
+  let sum = num1 + num2;
+  return sum 
+}
+console.log(sumOfTwo(10, 40)) // 50 
+
+let ans = sumOfTwo(100, 300)
+console.log(ans) // 400
+
+// function that returns product of two numbers
+function prdOfTwo(n1, n2){
+  return n1*n2
+}
+console.log(prdOfTwo(4, 25))
